f74676d2a9e051ce3ec88bcb2b7330f038c47d0c
middagh <juliomiddagh@gmail.com>
2023-11-16 00:34:40 -0300

    footer

--- MenuRestorante/src/App.jsx
+++ MenuRestorante/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
 
 const App = () => {
   return (
     <Container>
       <Header />
-
       <Row>
         <Col>
           <h1>Hello, React Bootstrap!</h1>
         </Col>
       </Row>
+      <Footer />
     </Container>
   );
 };
