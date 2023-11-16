37f681da167beadbf5b9b177f3d400c45ecb48d6
middagh <juliomiddagh@gmail.com>
2023-11-16 00:05:39 -0300

    add header

--- MenuRestorante/src/App.jsx
+++ MenuRestorante/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from '../src/components/Header';
 
 const App = () => {
   return (
     <Container>
+      <Header />
+
       <Row>
         <Col>
           <h1>Hello, React Bootstrap!</h1>
