3662754b944accb137db6abbab33fd67e9dbb49a
middagh <juliomiddagh@gmail.com>
2023-11-17 11:51:17 -0300

    fixing bugs

--- MenuRestorante/src/App.jsx
+++ MenuRestorante/src/App.jsx
@@ -3,10 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PublicRoutes from '../src/routes/PublicRoutes';
 import Home from '../src/pages/PublicPage/Home';
 /*import PrivateRoutes from './PrivateRoutes';*/
-
 
 const App = () => {
   return (
--- MenuRestorante/src/pages/PublicPage/Home.jsx
+++ MenuRestorante/src/pages/PublicPage/Home.jsx
@@ -2,21 +2,25 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../../assets/home.css'; 
+import fondoImagen from '../../assets/images/MainPicEmp.jpeg';
 
 const Home = () => {
   return (
-    <Container>
-      <Row>
+    <Container className='containerStyle'>
+      <Row className="mb-4">
         <Col>
-          <Card>
+          <Card className="mainCard text-white" style={{ backgroundImage: `url(${fondoImagen})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <Card.Body>
-              <Card.Title>Bienvenido a Los Hornos</Card.Title>
-              <Card.Text>
-                Descubre los sabores auténticos de nuestra cocina. ¡Te damos la bienvenida a una experiencia culinaria única!
-              </Card.Text>
-              <div className="text-center"> {/* Utiliza la clase 'text-center' para centrar el contenido */}
+              <div className="text-center">
+                <Card.Title>Bienvenido a Los Hornos</Card.Title>
+                <Card.Text>
+                  Descubre los sabores auténticos de nuestra cocina. ¡Te damos la bienvenida a una experiencia culinaria única!
+                </Card.Text>
                 <Link to="/menu">
-                  <Button variant="success">Ver Menú</Button>
+                  <Button variant="success" size="lg">
+                    Ver Menú
+                  </Button>
                 </Link>
               </div>
             </Card.Body>
@@ -24,13 +28,16 @@ const Home = () => {
         </Col>
       </Row>
 
-      <Row>
+      <Row className="mb-4 secondaryCard">
         <Col>
           <h2>Nuestra Historia</h2>
           <p>
             En "Los Hornos", nos enorgullece ofrecer platos auténticos y de la más alta calidad. Desde nuestros inicios en 1990, nos hemos comprometido a brindar una experiencia gastronómica excepcional a nuestros clientes.
           </p>
         </Col>
+      </Row>
+
+      <Row className="mb-4 secondaryCard">
         <Col>
           <h2>Nuestro Menú</h2>
           <p>
