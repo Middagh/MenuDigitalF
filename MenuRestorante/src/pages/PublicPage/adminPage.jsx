98c90245e7f30b28b7f13a71b9dcff9ea221ee38
middagh <juliomiddagh@gmail.com>
2023-12-20 18:15:13 -0300

    add product table

--- MenuRestorante/src/pages/PublicPage/adminPage.jsx
+++ MenuRestorante/src/pages/PublicPage/adminPage.jsx
@@ -7,8 +7,9 @@ import { Container, Table } from 'react-bootstrap';
 import pruebaApi from '../../api/Api';
 
 const AdminPage = () => {
-  // almacenamiento de los usuarios en front
-  const [uploadUser, setUploadUser] = useState([]);
+  const [uploadUser, setUploadUser] = useState([]);  // almacenamiento de los usuarios en front
+  const [uploadProduct, setUploadProduct] = useState([]);  // almacenamiento de los productos en front
+
   // trae a los usuarios del DB
   const bringUser = async () => {
     try {
@@ -19,11 +20,25 @@ const AdminPage = () => {
       console.log(error)
     }
   }
+
+  // trae a los productos del DB
+  const bringProduct = async () => {
+    try {
+      const resp = await pruebaApi.get("/admin/getproduct") // ruta de la db
+
+      setUploadProduct(resp.data.product); // guarda los usuarios traidos del db a la consola en front
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   //carga los usuarios bien se ingresa a la page
   useEffect(() => {
     bringUser();
+    bringProduct();
   }, [])
 
+ 
   return (
     <Container>
 
@@ -46,17 +61,45 @@ const AdminPage = () => {
           {
             uploadUser.map((user) => {
               return (
-              <tr key={user._id}>
-                <td>{user._id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-              </tr>
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                </tr>
 
               )
             })
           }
         </tbody>
       </Table>
+
+
+
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+          </tr>
+        </thead>        <tbody>
+          {
+            uploadProduct.map((product) => {
+              return (
+                <tr key={product._id}>
+                  <td>{product.name}</td>
+                  <td>{product._id}</td>
+                  <td>{product.price}</td>
+                  <td>{product.description}</td>
+                </tr>
+
+              )
+            })
+          }
+        </tbody>
+      </Table>
+
 
     </Container>
   );
